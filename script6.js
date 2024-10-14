8cb777f9b21b0e65744aa9cbc8bf90108813c739
Samuel do nascimento ramos <hmsrp.ramos7@gmail.com>
2024-10-14 17:05:03 -0300

    Add files via upload

---
+++ script6.js
@@ -0,0 +1,5 @@
+let carro_novo = parseInt(prompt("digite o valor do carro novo"))
+let distribuidor = 0.28
+let imposto = 0.45
+let resultado =  carro_novo + (carro_novo * distribuidor ) + (carro_novo * imposto)
+alert ("o valor do carro é:" + resultado)
